e7e72bccaeb40c3ef251ba7608073deb68deeefe
Esmeralda8806 <128433361+Esmeralda8806@users.noreply.github.com>
2023-04-18 16:26:28 -0300

    up

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// --validacion de formulario con jQuery--//
+$(document).ready(function() {
+  $("#basic-form").validate();});
+
+  // validacion de nombre//
+
+  function validar(){
+	//obtengo el valor del input 
+  //y elimino espacios al inicio y final del mismo
+	var nombre = $.trim($("#name").val());
+  //verifico si tiene un valor 
+	if(nombre == '' || nombre == null){
+  	$("#name").val(null);
+  	alert('Ingrese el nombre.');
+    return false;
+  }
+}//end function validar
