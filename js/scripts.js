917e7fb69dda5cae24a91ae6d1d384a4b83d2671
marcos <markdam85@gmail.com>
2023-04-19 15:05:56 -0300

    agregado script API

--- js/scripts.js
+++ js/scripts.js
@@ -70,5 +70,35 @@ $(document).ready(function () {
 });
 
 
+const HTMLResponse = document.querySelector("#app");
+const templateUL = document.createElement(`ul`);
+
+$.ajax({ method: "GET", url: "https://jsonplaceholder.typicode.com/comments" })
+
+    //users: es la RESPUESTA DE LA PETICION
+    .done(function (comments) {
+
+        //Mostrar por consola la respuesta de la API
+        console.log(comments)
+
+        //Mostrar respuesta de API en un componente HTML
+        //document.getElementById("app").innerHTML = users;
+
+        //Mostrar respuesta de API en un componente HTML recorriendo cada usuario
+        comments.forEach((comment) => {
+
+            let elem = document.createElement("li");
+            elem.appendChild(
+                document.createTextNode(`Nombre: ${comment.name}, Comentario: ${comment.body}, Email: ${comment.email}`)
+                
+            );
+            templateUL.appendChild(elem);
+        });
+        HTMLResponse.appendChild(templateUL);
+
+    }
+    )
+
+
 
 
